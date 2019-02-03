da68959d62ae27d13fbd50b810404441c822febc
Dhandapani R <drathinavel@paypal.com>
2019-02-04 00:54:03 +0530

    change forgot password back button in search

---
+++ ATC/ATC/View/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  ATC
+//
+//  Created by Rathinavel, Dhandapani(AWF) on 03/02/19.
+//  Copyright © 2019 Rathinavel, Dhandapani. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
